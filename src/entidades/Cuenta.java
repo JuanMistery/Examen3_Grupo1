0d6792cebe938135a069fc496cf2befdc6b97046
Weshlyn-22 <sorinsramoscarranza22@gmail.com>
2024-12-05 22:15:27 -0500

    Atributos de la clase cuenta

---
+++ src/entidades/Cuenta.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author 22ram
+ */
+public class Cuenta {
+    private int numeroCuenta;
+    private float saldoCuenta;
+    private String clave;
+    private GregorianCalendar fechaCreacion;
+    private int tipoDeCuenta;
+    private int tipoMonedaCuenta;
+}
