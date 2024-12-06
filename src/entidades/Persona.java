e2349953cfac1aa099be959b2b4abbb424544344
JuanMistery <juanfervs121@gmail.com>
2024-12-05 22:36:32 -0500

    Añado Atributos y metodos a la clase Persona

--- src/entidades/Persona.java
+++ src/entidades/Persona.java
@@ -9,5 +9,44 @@ package entidades;
  * @author JuanMistery
  */
 public abstract class Persona {
-    private String nombre;
+    private String nombreCompleto;
+    private String direccion;
+    private String telefono;
+    private String correoElectronico;
+
+    public Persona(String nombreCompleto, String direccion, String telefono, String correoElectronico) {
+        this.nombreCompleto = nombreCompleto;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+    
 }
