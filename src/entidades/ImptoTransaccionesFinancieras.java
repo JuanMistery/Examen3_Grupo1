35f07d441688038a2fff15453287692ee7b1f4a2
DJhonTR <djhontole14@gmail.com>
2024-12-09 19:46:21 -0500

    interfaz

---
+++ src/entidades/ImptoTransaccionesFinancieras.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author Sistemas
+ */
+public interface ImptoTransaccionesFinancieras {
+    public float ITF = 0.00005f;
+    
+    public float calcularITF(float cantidad);
+}
