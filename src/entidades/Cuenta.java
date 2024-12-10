91847791c8dc1fbc045ff8fc065a1fd9187ead30
DJhonTR <djhontole14@gmail.com>
2024-12-09 19:40:33 -0500

    Clase cuenta e interfaz itf

--- src/entidades/Cuenta.java
+++ src/entidades/Cuenta.java
@@ -4,6 +4,7 @@
  */
 package entidades;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -11,23 +12,28 @@ import java.util.GregorianCalendar;
  *
  * @author 22ram
  */
-public class Cuenta {
+public abstract class Cuenta implements ImptoTransaccionesFinancieras{
     private String numeroCuenta;
     private float saldoCuenta;
     private String clave;
+    Cliente titularDeCuenta;
     private GregorianCalendar fechaCreacion;
     private int tipoCuenta;
+    private int tipoDeIntereses;
     private int tipoMonedaCuenta;
-    private static int totalCuentasAhorro;
-    private static int totalCuentasCorriente;
+    ArrayList<Operaciones> operaciones = new ArrayList<Operaciones>();
+    private static int totalCuentasAhorro = 0;
+    private static int totalCuentasCorriente = 0;
     
     
     
-    public Cuenta(float saldoCuenta, String clave, int tipoCuenta) { 
+    public Cuenta (float saldoCuenta, String clave, int tipoCuenta, Cliente titularDeCuenta) { 
         this.tipoCuenta = tipoCuenta;
         this.saldoCuenta = saldoCuenta;
         this.clave = clave;
+        this.titularDeCuenta = titularDeCuenta;
         fechaCreacion = new GregorianCalendar();
+        
         if(tipoCuenta == 1)
             ++totalCuentasAhorro;
         else
@@ -35,10 +41,11 @@ public class Cuenta {
         numeroCuenta = generarNumeroCuenta();
     }
     
-    public Cuenta(String clave, int tipoCuenta) { 
+    public Cuenta(String clave, int tipoCuenta, Cliente titularDeCuenta) { 
         this.tipoCuenta = tipoCuenta;
         saldoCuenta = 0.0f;
         this.clave = clave;
+        this.titularDeCuenta = titularDeCuenta;
         fechaCreacion = new GregorianCalendar();
         if(tipoCuenta == 1)
             ++totalCuentasAhorro;
@@ -106,6 +113,54 @@ public class Cuenta {
     public static int obtenerTotalCuentasCorriente() {
         return totalCuentasCorriente;
     }
+
+    public Cliente getTitularDeCuenta() {
+        return titularDeCuenta;
+    }
+
+    public void setTitularDeCuenta(Cliente titularDeCuenta) {
+        this.titularDeCuenta = titularDeCuenta;
+    }
+
+    public int getTipoDeIntereses() {
+        return tipoDeIntereses;
+    }
+
+    public void setTipoDeIntereses(int tipoDeIntereses) {
+        this.tipoDeIntereses = tipoDeIntereses;
+    }
+
+    public int getTipoMonedaCuenta() {
+        return tipoMonedaCuenta;
+    }
+
+    public void setTipoMonedaCuenta(int tipoMonedaCuenta) {
+        this.tipoMonedaCuenta = tipoMonedaCuenta;
+    }
+
+    public static int getTotalCuentasAhorro() {
+        return totalCuentasAhorro;
+    }
+
+    public static void setTotalCuentasAhorro(int totalCuentasAhorro) {
+        Cuenta.totalCuentasAhorro = totalCuentasAhorro;
+    }
+
+    public static int getTotalCuentasCorriente() {
+        return totalCuentasCorriente;
+    }
+
+    public static void setTotalCuentasCorriente(int totalCuentasCorriente) {
+        Cuenta.totalCuentasCorriente = totalCuentasCorriente;
+    }
+
+    public ArrayList<Operaciones> getOperaciones() {
+        return operaciones;
+    }
+
+    public void setOperaciones(ArrayList<Operaciones> operaciones) {
+        this.operaciones = operaciones;
+    }
     
     @Override
     public String toString() {
@@ -125,4 +180,133 @@ public class Cuenta {
     public boolean validarClave(String clave) {
         return (getClave().compareTo(clave)==0);
     }
+     public class Operaciones {
+        private String numeroOperacion;
+        private GregorianCalendar fechaOperacion;
+        private int tipoOperacion;
+        private float montoOperacion;
+
+        public Operaciones(int tipoOperacion, float montoOperacion) {
+            numeroOperacion = generarNumeroOperacion();
+            fechaOperacion = new GregorianCalendar();
+            this.tipoOperacion = tipoOperacion;
+            this.montoOperacion = montoOperacion;
+        }
+
+        public String getNumeroOperacion() {
+            return numeroOperacion;
+        }
+
+        public GregorianCalendar getFechaOperacion() {
+            return fechaOperacion;
+        }
+
+        public void setTipoOperacion(int tipoOperacion) {
+            this.tipoOperacion = tipoOperacion;
+        }
+
+        public int getTipoOperacion() {
+            return tipoOperacion;
+        }
+
+        public void setMontoOperacion(float montoOperacion) {
+            this.montoOperacion = montoOperacion;
+        }
+        
+        public float getMontoOperacion() {
+            return montoOperacion;
+        }
+
+        @Override
+        public String toString() {
+            String tipo = "";
+            switch(tipoOperacion) {
+                case 1: tipo = "Deposito"; break;
+                case 2: tipo = "Retiro"; break;
+                case 3: tipo = "Transferencia"; break;
+                case 4: tipo = "ITF"; break;
+                case 5: tipo = "Interes mensual"; break;
+            }
+            return "\n\tNumero de operacion: " + getNumeroOperacion() +
+                   "\n\tFecha: " + getFechaOperacionCorta() + 
+                   "\n\tTipo: " + tipo + "\n\tMonto: " + getMontoOperacion();
+        }
+        
+        public String getFechaOperacionCorta() {
+            int dia, mes, anio;
+            dia = getFechaOperacion().get(Calendar.DAY_OF_MONTH);
+            mes = getFechaOperacion().get(Calendar.MONTH)+1;
+            anio = getFechaOperacion().get(Calendar.YEAR);
+            return (dia<=9?"0"+dia:dia) + "/" + (mes<=9?"0"+mes:mes) + "/" + anio;
+        }
+        
+        private String generarNumeroOperacion() {
+            int numDig=0, num=0, dato=0;
+            String numCuenta = "";
+            num = dato = getOperaciones().size();
+            while (num > 9) {
+                numDig++;
+                num /= 10;
+            }
+            numDig++;
+            for(int i=0;i<10-numDig; i++)
+                numCuenta += "0";
+            numCuenta += dato;        
+            return numCuenta;
+        }
+            public boolean realizarDeposito(float montoD) {
+        Operaciones movimiento;
+        setSaldoCuenta(getSaldoCuenta() + montoD);
+        movimiento = new Operaciones(1, montoD);
+        operaciones.add(movimiento);
+        if(montoD >= 1000) {
+            setSaldoCuenta(getSaldoCuenta() - calcularITF(montoD));
+            movimiento = new Operaciones(4, calcularITF(montoD));
+            operaciones.add(movimiento);
+        }
+        return true;
+    }
+    
+    public boolean realizarRetiro(float montoR) {
+        Operaciones movimiento;
+        float montoITF = calcularITF(montoR);
+        if(montoR >= 1000) {
+            if(montoR + montoITF <= getSaldoCuenta()) {
+                setSaldoCuenta(getSaldoCuenta() - montoR);
+                movimiento = new Operaciones(2, montoR);
+                operaciones.add(movimiento);
+                setSaldoCuenta(getSaldoCuenta() - montoITF);
+                movimiento = new Operaciones(4, montoITF);
+                operaciones.add(movimiento);
+                return true;
+            }
+            return false;
+        } else {
+            if(montoR <= getSaldoCuenta()) {
+                setSaldoCuenta(getSaldoCuenta() - montoR);
+                movimiento = new Operaciones(2, montoR);
+                operaciones.add(movimiento);
+                return true;
+            }
+        }
+        return false;
+    }
+           
+     public boolean realizarTransferencia(float montoTrans){
+         
+     }
+     public boolean emisionCheques(float montoCheque){
+         
+     }
+     public boolean cobroCheques(float montoCheque){
+         
+     }
+     public boolean verificacionCheques(String codigoCheque){
+         
+     }
+   }  
 }
+
+    
+     
+
