626fc7f96d71d807afdc611b88308598c5aeed9d
pipe_ <pipe_@Pipe>
2024-12-12 10:35:12 -0500

    Juancito aca tienes lista clientes

---
+++ src/datos/ListaClientes.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datos;
+
+import entidades.Cliente;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Misterioso
+ */
+public class ListaClientes {
+    private ArrayList<Cliente> listaClientes = new ArrayList<>();
+     
+    public ArrayList<Cliente> getListaClientes() {
+        return listaClientes;
+    }
+    
+    public boolean agregarCliente(Cliente cliente) {
+        if(cliente != null) {
+            listaClientes.add(cliente);
+            return true;
+        }
+        return false;
+    }
+    
+    public Cliente obtenerCliente(int pos) {
+        if(pos>=0 && pos<listaClientes.size()) 
+            return listaClientes.get(pos);
+        return null;
+    }
+    
+    public int buscarPorApellido(String apellido) {
+        Cliente clienteB;
+        for(int i=0; i<listaClientes.size(); i++) {
+            clienteB = listaClientes.get(i);
+            if(clienteB.getApellidos().compareToIgnoreCase(apellido)==0)
+                return i;
+        }
+        return -1;
+    }
+    
+    public int getTamanio() {
+        return listaClientes.size();
+    }            
+}
