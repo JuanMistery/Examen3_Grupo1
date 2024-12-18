a2421ebab7333097c33d837240d1ae655c388255
JuanMistery <juanfervs121@gmail.com>
2024-12-17 20:13:01 -0500

    Actualizacion de Cuenta Corriente

--- src/entidades/CuentaCorriente.java
+++ src/entidades/CuentaCorriente.java
@@ -10,15 +10,18 @@ package entidades;
  */
 public class CuentaCorriente extends Cuenta{
     private float limiteDeGiro;
+    private int LimiteDeCheques;
 
-    public CuentaCorriente(float limiteDeGiro, float saldoCuenta, String clave, int tipoCuenta, Cliente titularDeCuenta) {
+    public CuentaCorriente(float limiteDeGiro, int LimiteDeCheques, float saldoCuenta, String clave, int tipoCuenta, Cliente titularDeCuenta) {
         super(saldoCuenta, clave, tipoCuenta, titularDeCuenta);
         this.limiteDeGiro = limiteDeGiro;
+        this.LimiteDeCheques = LimiteDeCheques;
     }
 
-    public CuentaCorriente(float limiteDeGiro, String clave, int tipoCuenta, Cliente titularDeCuenta) {
+    public CuentaCorriente(float limiteDeGiro, int LimiteDeCheques, String clave, int tipoCuenta, Cliente titularDeCuenta) {
         super(clave, tipoCuenta, titularDeCuenta);
         this.limiteDeGiro = limiteDeGiro;
+        this.LimiteDeCheques = LimiteDeCheques;
     }
 
     public float getLimiteDeGiro() {
@@ -29,10 +32,12 @@ public class CuentaCorriente extends Cuenta{
         this.limiteDeGiro = limiteDeGiro;
     }
 
-    @Override
-    public float calcularITF(float cantidad) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public int getLimiteDeCheques() {
+        return LimiteDeCheques;
     }
-    
-    
+
+    public void setLimiteDeCheques(int LimiteDeCheques) {
+        this.LimiteDeCheques = LimiteDeCheques;
+    }
+ 
 }
